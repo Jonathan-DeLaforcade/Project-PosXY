e04529d767dbae24873bc4aec44c2d6ac5fca252
Jonathan De Laforcade <delaforcade@lacroixrouge-brest.fr>
2019-01-11 18:07:12 +0100

    On branch master  Changes to be committed: 	modified:   Canvas_bouger_cube.py 	modified:   data.txt

--- Canvas_bouger_cube.py
+++ Canvas_bouger_cube.py
@@ -5,7 +5,7 @@ coords = (0, 0)
 
 def mouseClick(event):
     global coords
-    if (event.x >= 0) & (event.y >= 0) & (event.x <= 250) & (event.y <= 250):
+    if (event.x >= 0) & (event.y >= 0) & (event.x <= 236) & (event.y <= 236):
         coords = (event.x,event.y)
         print("Mouse position: (%s %s)" % (coords[0], coords[1]))
         canvas.coords(rectangle, coords[0], coords[1], coords[0]+15, coords[1]+15)
